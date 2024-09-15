a46d3fe24d02bf1b58e3ea2eed3889de022bd33d
Allar Kalina <allarkalina@gmail.com>
2024-09-15 18:23:52 +0300

    Fix #2

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 
+const port = 3000;
+const baseURL = `http://localhost:${port}`;
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -36,7 +39,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -82,10 +85,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "pnpm dev",
-    url: "http://localhost:3000",
+    port,
+    command: "yarn dev",
     reuseExistingServer: !process.env.CI,
-    stdout: "ignore",
-    stderr: "pipe",
   },
 });
